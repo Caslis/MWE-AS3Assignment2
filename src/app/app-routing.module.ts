494ab3390098cd9312dc24a13e36e2c7af824874
Amirul <22FTT1493@student.pb.edu.bn>
2024-04-05 08:40:43 +0800

    Add routing for Tab1Page and Tab2Page (backbutton)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Tab1Page } from './tab1/tab1.page'; // Import the 'Tab1Page' component
+import { Tab2Page } from './tab2/tab2.page'; // Import the 'Tab2Page' component
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },
+  { path: 'tab1', component: Tab1Page },
+  { path: 'tab2', component: Tab2Page },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
