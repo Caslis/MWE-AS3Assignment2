8d35973d65ad1a993993bfeda97f60f3196003b6
Amirul <22FTT1493@student.pb.edu.bn>
2024-04-06 09:15:23 +0800

    Update tab2 page with about section and menu styling

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonMenu } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: './tab2.page.html',
-  styleUrls: ['./tab2.page.scss'],
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page {
   constructor() {}
 
-  ngOnInit() {}
+  // Method to close the menu
+  closeMenu() {
+    const menu = document.querySelector('ion-menu') as HTMLIonMenuElement;
+    menu.close(); // Close the menu
+  }
 }
